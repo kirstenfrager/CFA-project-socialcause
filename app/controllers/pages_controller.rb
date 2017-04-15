66a7a08de30c461e9fbdbbe580520e87e51b3b0a
Kirsten Frager <kirstenfrager@192-168-1-4.tpgi.com.au>
2017-04-15 22:43:30 +1000

    instagram authorisation added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def callback
+    response = Instagram.get_access_token(params[:code], redirect_uri: ENV['REDIRECT_URI'])
+    session[:access_token] = response.access_token
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get 'influencers_profile/show'
 
   root 'pages#home'
+  get '/callback' => 'pages#callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
