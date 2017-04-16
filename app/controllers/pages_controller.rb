83792ca8e3c6d080cb09d29c6bb794aac9f440d9
Kirsten Frager <kirstenfrager@192-168-1-4.tpgi.com.au>
2017-04-16 13:13:22 +1000

    extra insta features

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
-  def home
-  end
 
   def callback
+    # binding.pry
     response = Instagram.get_access_token(params[:code], redirect_uri: ENV['REDIRECT_URI'])
     session[:access_token] = response.access_token
+    flash[:notice] = "Successfully logged in with Instagram"
     redirect_to root_path
   end
+
+#   def home
+#     @instagram = Instagram.user_recent_media("745531022", {:count => 1})
+# end
 end
