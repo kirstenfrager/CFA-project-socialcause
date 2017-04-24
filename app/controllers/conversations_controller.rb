fde6bb14a6ceabeb6b08da19fe5605ffb8c79151
Kirsten Frager <kirstenfrager@Kirstens-MacBook-Air.local>
2017-04-24 20:19:33 +1000

    styling of messages and inbox with subject header

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,6 +4,7 @@ class ConversationsController < ApplicationController
   def index
     @users = User.all
     @conversations = Conversation.all
+    @messages = Message.all
   end
 
   # GET /Conversation/new
