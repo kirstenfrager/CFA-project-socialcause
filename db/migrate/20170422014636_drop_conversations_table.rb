00358aa3443dc39c1abe75068e00fb5f1eb398cc
Kirsten Frager <kirstenfrager@Kirstens-MacBook-Air.local>
2017-04-24 16:42:33 +1000

    conversation migration file

--- db/migrate/20170422014636_drop_conversations_table.rb
+++ db/migrate/20170422014636_drop_conversations_table.rb
@@ -1,10 +1,10 @@
-# class DropConversationsTable < ActiveRecord::Migration[5.0]
-#   def change
-#     drop_table :conversations do |t|
-#       t.integer :sender_id
-#       t.integer :recipient_id
-#
-#       t.timestamps
-#     end
-#   end
-# end
+class DropConversationsTable < ActiveRecord::Migration[5.0]
+  # def change
+  #   drop_table :conversations do |t|
+  #     t.integer :sender_id
+  #     t.integer :recipient_id
+  #
+  #     t.timestamps
+  #   end
+  # end
+end
