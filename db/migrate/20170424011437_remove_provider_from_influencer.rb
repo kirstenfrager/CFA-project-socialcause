6f68bc8c6975f2403cadb9e3d2fa4d6b3eb4742c
Kirsten Frager <kirstenfrager@Kirstens-MacBook-Air.local>
2017-04-24 16:44:40 +1000

    column migrations fixed

--- db/migrate/20170424011437_remove_provider_from_influencer.rb
+++ db/migrate/20170424011437_remove_provider_from_influencer.rb
@@ -1,5 +1,5 @@
 class RemoveProviderFromInfluencer < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :influencers, :provider
-  end
+  # def change
+  #   remove_column :influencers, :provider
+  # end
 end
